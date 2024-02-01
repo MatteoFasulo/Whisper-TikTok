accf7054a96cd86059114ac86745a1f7b791c1cb
MatteoFasulo <74818541+MatteoFasulo@users.noreply.github.com>
2024-02-01 18:01:27 +0100

    Refactor translation and optimization process for Reddit posts

--- reddit2json.py
+++ reddit2json.py
@@ -17,22 +17,25 @@ load_dotenv()  # take environment variables from .env.
 parser = argparse.ArgumentParser(description='Process Reddit posts.')
 parser.add_argument('--method', type=str, default='chat', choices=['translate', 'chat'],
                     help='Method to use for processing text. "translate" uses Deepl, "chat" uses GPT-3.5 Turbo.')
-parser.add_argument('--lang', type=str, default='DE',
+parser.add_argument('--lang', type=str, default='EN',
                     help='Target language for translation. Only used if method is "translate".')
 args = parser.parse_args()
 
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+
 def chat_with_gpt3(prompt):
     completion = client.chat.completions.create(
-      model="gpt-3.5-turbo",
-      messages=[
+        model="gpt-3.5-turbo",
+        messages=[
             {"role": "system", "content": f"You are an assistant that meaningfully translates English Reddit post texts into Language:{args.lang} and optimizes them for text-to-speech. The following is a Reddit post that you should translate and optimize for text-to-speech"},
             {"role": "user", "content": prompt}
         ]
     )
-    return completion.choices[0].message.content  # Extract the content attribute
+    # Extract the content attribute
+    return completion.choices[0].message.content
+
 
 def get_reddit_post(url):
     reddit = praw.Reddit(
@@ -45,7 +48,7 @@ def get_reddit_post(url):
 
 
 def translate_to_german(text):
-    #url = "https://api.deepl.com/v2/translate"
+    # url = "https://api.deepl.com/v2/translate"
     url = "https://api-free.deepl.com/v2/translate"
     data = {
         "auth_key": os.getenv("DEEPL_AUTH_KEY"),
@@ -62,10 +65,11 @@ def process_text(title, text):
         title = translate_to_german(title)
         text = translate_to_german(text)
     elif args.method == 'chat':
-        title = chat_with_gpt3(f"Translate the following title into Language: {args.lang} and adjust it so that it is optimized for a lecture by a text-to-speech program. Also remove all parentheses such as (29m) or (M23) or (M25) etc. Also remove all edits from the Reddit post so only the pure text remains:" + "\n\n" + "title" + "\n\n" + "Revised title:")
-        text = chat_with_gpt3(f"Translate the following text into Language: {args.lang} and adjust it so that it is optimized for a lecture by a text-to-speech program. Also remove all parentheses such as (29m) or (M23) or (M25) or (19) etc. Also remove all edits from the Reddit post so only the pure text remains. Break off the text at the most exciting point to keep the readers very curious:" + "\n\n" + "text" + "\n\n" + "Revised text:")
+        title = chat_with_gpt3(
+            f"Translate the following title into Language: {args.lang} and adjust it so that it is optimized for a lecture by a text-to-speech program. Also remove all parentheses such as (29m) or (M23) or (M25) etc. Also remove all edits from the Reddit post so only the pure text remains:" + "\n\n" + "title" + "\n\n" + "Revised title:")
+        text = chat_with_gpt3(
+            f"Translate the following text into Language: {args.lang} and adjust it so that it is optimized for a lecture by a text-to-speech program. Also remove all parentheses such as (29m) or (M23) or (M25) or (19) etc. Also remove all edits from the Reddit post so only the pure text remains. Break off the text at the most exciting point to keep the readers very curious:" + "\n\n" + "text" + "\n\n" + "Revised text:")
     return title, text
-
 
 
 def modify_json(title_text, part_text, outro_text, main_text):
@@ -77,19 +81,22 @@ def modify_json(title_text, part_text, outro_text, main_text):
             "outro": outro_text[i],
             "text": main_text[i]
         })
-    
+
     with open('./video.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False)
+
 
 def read_file_line_by_line(file_path):
     with open(file_path, 'r') as file:
         for line in file:
             yield line
 
+
 title_text = []
 main_text = []
 
-lines = list(read_file_line_by_line('./reddit-post.txt'))  # Convert generator to list to get length
+# Convert generator to list to get length
+lines = list(read_file_line_by_line('./reddit-post.txt'))
 
 for line in tqdm(lines, desc="Processing Reddit posts", unit="post"):
     title, text = get_reddit_post(line)
@@ -121,7 +128,6 @@ for line in tqdm(lines, desc="Processing Reddit posts", unit="post"):
 # Initialize part_text and outro_text after the loop
 part_text = [""] * len(title_text)
 outro_text = [""] * len(title_text)
-
 
 
 modify_json(title_text, part_text, outro_text, main_text)

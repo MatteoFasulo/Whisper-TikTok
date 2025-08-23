5d5cc9ed8d05f42d878e87836473b485ea6e12f7
Matteo Fasulo <mat.fasulo@gmail.com>
2025-08-23 19:46:04 +0200

    Update with pycodestyle, pylint, mypy, riff and black formatting checks and improved code structure
    Signed-off-by: Matteo Fasulo <mat.fasulo@gmail.com>

--- whisper_tiktok/video_downloader.py
+++ whisper_tiktok/video_downloader.py
@@ -1,3 +1,5 @@
+"""Module for downloading videos using yt-dlp from YouTube."""
+
 import os
 import subprocess
 from pathlib import Path
@@ -11,12 +13,11 @@ def download_video(url: str, folder: str = "background"):
 
     Args:
         url (str): The URL of the video to download.
-        folder (str, optional): The name of the folder to save the video in. Defaults to 'background'.
+        folder (str, optional): The name of the folder to save the video in.
     """
     directory = HOME / folder
     os.makedirs(directory, exist_ok=True)
 
-    subprocess.Popen(
-        ["yt-dlp", "-f bestvideo[ext=mp4]", "--restrict-filenames", url], cwd=directory
-    ).wait()
-    return
+    cmd = ["yt-dlp", "-f bestvideo[ext=mp4]", "--restrict-filenames", url]
+    with subprocess.Popen(cmd, cwd=directory) as process:
+        process.wait()
